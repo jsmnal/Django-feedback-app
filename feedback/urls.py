2fdaed1e12659a7e1219a26d96e1fef6dd8cef1c
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-07-14 12:06:46 +0300

    Add and update templates and urls

--- feedback/urls.py
+++ feedback/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("", views.index, name="index"),
     path("sign_in", views.sign_in, name="sign_in"),
+    path("sign_out", views.sign_out, name="sign_out"),
     path("create_feedback/", views.CreateFeedbackForm.as_view(), name="create_feedback"),
     path("delete_feedback/<pk>/", views.DeleteFeedbackForm.as_view(), name="delete_feedback"),
     path("update_feedback/<pk>/", views.UpdateFeedbackForm.as_view(), name="update_feedback"),
