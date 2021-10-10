e82ef071130ffb3bb27e5e4e866e21cabf7e5d53
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-10-10 16:35:26 +0300

    Update models

--- feedback/models.py
+++ feedback/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Message(models.Model):
     title = models.CharField('title', max_length=100)
+    course = models.ForeignKey('Course', verbose_name='course', on_delete = models.SET_NULL, null=True)
     teacher = models.ForeignKey('Teacher', verbose_name='teacher', on_delete = models.SET_NULL, null=True)
     rating = models.IntegerField('rating')
     feedback = models.CharField('feedback', max_length=300)
@@ -18,7 +19,21 @@ class Teacher(models.Model):
     last_name = models.CharField('last name', max_length=100)
 
     def __str__(self):
-        return f"{self.first_name}/{self.last_name}"
+        return f"{self.first_name} {self.last_name}"
+
+class Course(models.Model):
+    title = models.CharField('title', max_length=100)
+    teacher = models.ForeignKey('Teacher', verbose_name='teacher', on_delete = models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.teacher}/{self.title}"
+
+class Bug(models.Model):
+    title = models.CharField('title', max_length=100)
+    message = models.CharField('message', max_length=300)
+
+    def __str__(self):
+        return self.title
 
 class User(AbstractUser):
     pass
